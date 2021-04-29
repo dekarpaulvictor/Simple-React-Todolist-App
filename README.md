ec0f86744382a912adb4e0a60a2625d7a6920f69
dekarpaulvictor <dekarpaulvictor@gmail.com>
2021-04-29 22:43:48 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple React Todolist App
+
+This is just a simple todolist app built using [React](https://react.org).
+It has no styling. The point was to create and test all the basic functions
+a decent todolist app has.
+
+### Features
+
+Apart from the usual (adding and marking and crossing off tasks as complete) you can:
+  - Delete a task whether it is complete or not
+  - Show all tasks
+  - Hide completed tasks
+  - Hide incomplete tasks
+
+### Running locally
+
+1. Clone this github repo: `git clone https://github.com/dekarpaulvictor/SimpleReactTodolistApp.git`
+2. Change into the root directory and run `yarn start` or `npm run start`
+
+The app will be served at `http://localhost:3000`
---
+++ src/App.jsx
@@ -0,0 +1,181 @@
+import {useState} from 'react';
+import {nanoid} from 'nanoid';
+
+const initialTodos = [
+  {
+    id: nanoid(),
+    task: "Make breakfast",
+    done: true,
+    hidden: false
+  },
+  {
+    id: nanoid(),
+    task: "Take a shower",
+    done: false,
+    hidden: false
+  },
+  {
+    id: nanoid(),
+    task: "Dress and go to work",
+    done: true,
+    hidden: false
+  },
+  {
+    id: nanoid(),
+    task: "Slap boss",
+    done: false,
+    hidden: false
+  },
+];
+
+const TodoList = ({list, onTaskDone, onTaskDelete}) => {
+  return (
+    <ul>
+      {
+        list.map((item => (
+          <li
+            key={item.id}
+            style={
+              {
+                textDecoration: `${item.done ? 'line-through' : ''}`
+              }
+            }
+            hidden={item.hidden}
+          >
+            {item.task}
+            <input 
+              type="checkbox" 
+              checked={item.done} 
+              onChange={() => onTaskDone(item.id)}
+            />
+            <button 
+              onClick={() => onTaskDelete(item.id)}>
+              Delete
+            </button>
+          </li>
+        )))
+      }
+    </ul>
+  );
+}
+
+const TodoApp = () => {
+
+  const [todos, setTodos] = useState(initialTodos);
+  const [text, setText] = useState('');
+  
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  }
+  
+  const handleCheckboxState = (id) => {
+    // Search for todo item with matching id to change
+    // it's done status
+    setTodos(
+      todos.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            done: !item.done
+          }
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  const handleTaskDelete = (id) => {
+    // Search todos and delete item with matching id
+    setTodos(
+      todos.filter(item => item.id !== id)
+    );
+  }
+
+  const handleShowAll = () => {
+    // Search todos and toggle value of `hidden` property if true
+    setTodos(
+      todos.map(item => {
+        if (item.hidden) {
+          return {
+            ...item,
+            hidden: !item.hidden
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  const handleShowDone = () => {
+    // Search todos and change `hidden` value of incomplete tasks to true
+    setTodos(
+      todos.map(item => {
+        if (item.done) {
+          return {
+            ...item,
+            hidden: false
+          };
+        } else{
+          return {
+            ...item,
+            hidden: true
+          }
+        }
+      })
+    );
+  }
+
+  const handleShowNotDone = () => {
+    // Search todos and change value of `hidden` property to true
+    // if task is not done
+    setTodos(
+      todos.map(item => {
+        if (!item.done) {
+          return {
+            ...item,
+            hidden: false
+          };
+        } else{
+          return {
+            ...item,
+            hidden: true
+          }
+        }
+      })
+    );
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (text === '') return;
+    setTodos(todos.concat({
+      id: nanoid(),
+      task: text,
+      done: false,
+      hidden: false
+    })); 
+
+    setText('');
+  }
+
+  return (
+    <div>
+      <TodoList list={todos} onTaskDone={handleCheckboxState} onTaskDelete={handleTaskDelete} />
+      <span>
+        <button onClick={handleShowAll}>All</button>
+        <button onClick={handleShowDone}>Done</button>
+        <button onClick={handleShowNotDone}>Not done</button>
+      </span>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={text} onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default function App() {
+  return <TodoApp />;
+}
